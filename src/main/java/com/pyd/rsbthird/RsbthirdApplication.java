7db3d68e294b1eecec55d969f9d6465959cad3c2
AdamPyd <285307542@qq.com>
2024-03-30 10:17:23 +0800

    [项目初始化]-3-springboot 项目初始化文件，包含src、HELP和可不依赖全局的mvn配置

---
+++ src/main/java/com/pyd/rsbthird/RsbthirdApplication.java
@@ -0,0 +1,13 @@
+package com.pyd.rsbthird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsbthirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsbthirdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pyd/rsbthird/RsbthirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyd.rsbthird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsbthirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
