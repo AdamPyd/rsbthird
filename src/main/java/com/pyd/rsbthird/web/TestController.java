6d138dad365932ac8bfb7c891e72af2f8e9017d9
AdamPyd <285307542@qq.com>
2024-03-30 10:27:13 +0800

    [微信自动回复机器人]-1-测试master分支的保护策略

---
+++ src/main/java/com/pyd/rsbthird/web/TestController.java
@@ -0,0 +1,12 @@
+package com.pyd.rsbthird.web;
+
+/**
+ * @ClassName TestController
+ * @Package com.pyd.rsbthird.web
+ * @Description TODO
+ * @Author adam
+ * @Date 2024-03-30 10:25
+ * @Version 1.0.0
+ **/
+public class TestController {
+}
